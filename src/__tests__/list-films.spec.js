bf1154f47bc516e35e77224b840d81246930d683
Lawrence Siden <lsiden@gmail.com>
2018-03-08 17:39:59 -0500

    ListFilms renders list of titles

--- src/__tests__/list-films.spec.js
+++ src/__tests__/list-films.spec.js
@@ -2,11 +2,23 @@ import React from 'react'
 import ListFilms from 'list-films'
 import { shallow } from 'enzyme'
 
+import 'test-helpers/setup'
+
+const films = require('./films.json').Search
 const defaultProps = () => ({
+	films
 })
 
-const createWrapper = (props={}) => <ListFilms />
+const createWrapper = (props={}) => shallow(
+	<ListFilms {...{...defaultProps(), ...props}} />
+)
 
 it('renders', () => {
 	const wrapper = createWrapper()
+})
+
+it('renders list of titles', () => {
+	const wrapper = createWrapper()
+	const titles = wrapper.find('Title')
+	expect(titles).toHaveLength(films.length)
 })
--- src/list-films.js
+++ src/list-films.js
@@ -1,11 +1,20 @@
 import React from 'react'
-// import PropTypes from 'prop-types'
+import PropTypes from 'prop-types'
 
-const ListFilms = () => {
-	return <ul>
-		<li>A Title</li>
-		<li>Another Title</li>
-	</ul>
+function Title({film}) {
+	return "A title"
+}
+
+Title.propTypes = {
+	film: PropTypes.object.isRequired
+}
+
+const ListFilms = ({films=[]}) => <ul>{
+	films.map(film => (<li key={film.imdbID}><Title film={film} /></li>))
+}</ul>
+
+ListFilms.propTypes = {
+	films: PropTypes.arrayOf(PropTypes.object)
 }
 
 export default ListFilms
