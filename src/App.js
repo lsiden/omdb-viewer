f4d177c1d368e5aa4ddd1dbbfecd821c7a7b9802
Lawrence Siden <lsiden@gmail.com>
2018-03-08 20:30:07 -0500

    Connects Redux store to Provider

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 
 import logo from './logo.svg'
@@ -10,20 +11,6 @@ import './App.css'
 
 // FIXME - for demo only
 const films = require('./__tests__/films.json').Search
-
-class App extends Component {
-	render() {
-		return (
-			<div className="App">
-			<header className="App-header">
-			<img src={logo} className="App-logo" alt="logo" />
-			<h1 className="App-title">Welcome to React</h1>
-			</header>
-			<ListFilms films={films} />
-			</div>
-		)
-	}
-}
 
 const initialState = { view: Actions.OPEN_DETAIL }
 
@@ -43,5 +30,21 @@ export function reduce(state=initialState, action) {
 }
 
 const store = createStore(reduce, applyMiddleware(thunk))
+
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<h1 className="App-title">Welcome to React</h1>
+				</header>
+				<Provider store={store}>
+					<ListFilms films={films} />
+				</Provider>
+			</div>
+		)
+	}
+}
 
 export default App
