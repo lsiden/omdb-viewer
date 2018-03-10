5d131c965a29126ae6922c1dc89a17d58007e2ad
Lawrence Siden <lsiden@gmail.com>
2018-03-10 14:55:40 -0500

    Refactors display view render()

--- src/film-detail.js
+++ src/film-detail.js
@@ -25,8 +25,10 @@ const closeButtonStyle = {
 
 export class FilmDetail extends React.Component {
 	constructor(props) {
+		const { imdbID } = props.filmDetails
 		super(props)
 		this.keyDownListener = this.keyDownListener.bind(this)
+		this.imdbUrl = `https://www.imdb.com/title/${imdbID}`
 	}
 
 	keyDownListener(ev) {
@@ -43,48 +45,58 @@ export class FilmDetail extends React.Component {
 		document.removeEventListener('keydown', this.keyDownListener)
 	}
 
+	renderTitle() {
+		const { dispatchViewList, filmSummary } = this.props
+		return (
+			<div style={headerStyle}>
+				<h1 style={titleStyle}>{filmSummary.Title}</h1>
+				<a
+					href='#'
+					onClick={() => dispatchViewList()}
+					id="close-button"
+					title="Close"
+					style={closeButtonStyle}
+				>{'Close'}</a>
+			</div>
+		)
+	}
+
+	renderDetails() {
+		const { filmSummary, filmDetails } = this.props
+		return (
+			<ul style={detailsStyle}>
+				<li>{filmSummary.Year}</li>
+				<li>Directed by {filmDetails.Director}</li>
+				<li>Written by {filmDetails.Writer}</li>
+				<li>Cast: {filmDetails.Actors}</li>
+				<li>Language: {filmDetails.Language}</li>
+				{
+					filmDetails.Awards && <li>Awards: {filmDetails.Awards}</li>
+				}
+				<li>Run Time: {filmDetails.Runtime}</li>
+				<li>IMDB Rating: {filmDetails.imdbRating}/10</li>
+				<li>Box Office: {filmDetails.BoxOffice}</li>
+				{
+					filmDetails.Website && (
+						<li><a href={filmDetails.Website}>Official website</a></li>
+					)
+				}
+				<li><a href={this.imdbUrl}>{'IMDB page'}</a></li>
+			</ul>
+		)
+	}
+
 	render() {
-		const {
-			filmSummary,
-			filmDetails,
-			dispatchViewList
-		} = this.props
-		const imdbUrl = `https://www.imdb.com/title/${filmDetails.imdbID}`
+		const { filmSummary } = this.props
 		return (
 			<div style={wrapperStyle}>
 				<a name="top" />
-				<div style={headerStyle}>
-					<h1 style={titleStyle}>{filmSummary.Title}</h1>
-					<a
-						href='#'
-						onClick={() => dispatchViewList()}
-						id="close-button"
-						title="Close"
-						style={closeButtonStyle}
-					>{'Close'}</a>
-				</div>
+				{this.renderTitle()}
 				<img src={filmSummary.Poster} />
-				<ul style={detailsStyle}>
-					<li>{filmSummary.Year}</li>
-					<li>Directed by {filmDetails.Director}</li>
-					<li>Written by {filmDetails.Writer}</li>
-					<li>Cast: {filmDetails.Actors}</li>
-					<li>Language: {filmDetails.Language}</li>
-					{
-						filmDetails.Awards && <li>Awards: {filmDetails.Awards}</li>
-					}
-					<li>Run Time: {filmDetails.Runtime}</li>
-					<li>IMDB Rating: {filmDetails.imdbRating}/10</li>
-					<li>Box Office: {filmDetails.BoxOffice}</li>
-					{
-						filmDetails.Website && (
-							<li><a href={filmDetails.Website}>Official website</a></li>
-						)
-					}
-					<li><a href={imdbUrl}>{'IMDB page'}</a></li>
-				</ul>
-				<br />
-				<a href="#top">Top</a>
+				{this.renderDetails()}
+				<div>
+					<a href="#top">Top</a>
+				</div>
 			</div>
 		)
 	}
