4758e9df6746693debbb5ac25bfd73c277274355
Lawrence Siden <lsiden@gmail.com>
2018-03-10 16:21:23 -0500

    Changes color of "no films" message to match content.

--- src/film-list.js
+++ src/film-list.js
@@ -2,19 +2,20 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 
-import FilmTitle from 'film-title'
+import FilmTitle from './film-title'
+import { TITLE_COLOR } from './constants'
 
 const ulStyle = {
 	listStyleType: 'none',
 	lineHeight: 1.5,
 }
+const msgStyle = {
+	fontSize: '14pt',
+	color: TITLE_COLOR,
+	margin: '1em'
+}
 
 export const FilmList = ({films}) => {
-	const msgStyle = {
-		fontSize: '14pt',
-		color: '#3266ba',
-		margin: '1em'
-	}
 	if (films.length > 0) {
 		return (
 			<ul style={ulStyle}>{
@@ -28,7 +29,11 @@ export const FilmList = ({films}) => {
 			</ul>
 		)
 	} else {
-		return <div style={msgStyle}>There are no films that match your query yet.</div>
+		return (
+			<div style={msgStyle}>
+				{'There are no films that match your query yet.'}
+			</div>
+		)
 	}
 }
 
