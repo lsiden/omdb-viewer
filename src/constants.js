9be12129b85a2d6fdce0ae5a7a7c7502e3ed702f
Lawrence Siden <lsiden@gmail.com>
2018-03-10 14:41:19 -0500

    Adds ESC key listener to detail view

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,4 @@
 export const APP_NAME = 'omdb-viewer'
 export const OMD_URL = 'https://www.omdbapi.com/'
 export const QUERY_DELAY = 300
+export const ESC_KEY = 27
--- src/film-detail.js
+++ src/film-detail.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 
 import { viewList } from './actions'
+import { ESC_KEY } from './constants'
 
 const wrapperStyle = {
 	marginLeft: '10pt',
@@ -22,45 +23,71 @@ const closeButtonStyle = {
 	margin: '1em',
 }
 
-export const FilmDetail = ({filmSummary, filmDetails, dispatchViewList}) => {
-	const imdbUrl = `https://www.imdb.com/title/${filmDetails.imdbID}`
-	return <div style={wrapperStyle}>
-		<a name="top" />
-		<div style={headerStyle}>
-			<h1 style={titleStyle}>{filmSummary.Title}</h1>
-			<a
-				href='#'
-				onClick={() => dispatchViewList()}
-				id="close-button"
-				title="Close"
-				style={closeButtonStyle}
-			>
-				Close
-			</a>
-		</div>
-		<img src={filmSummary.Poster} />
-		<ul style={detailsStyle}>
-			<li>{filmSummary.Year}</li>
-			<li>Directed by {filmDetails.Director}</li>
-			<li>Written by {filmDetails.Writer}</li>
-			<li>Cast: {filmDetails.Actors}</li>
-			<li>Language: {filmDetails.Language}</li>
-			{
-				filmDetails.Awards && <li>Awards: {filmDetails.Awards}</li>
-			}
-			<li>Run Time: {filmDetails.Runtime}</li>
-			<li>IMDB Rating: {filmDetails.imdbRating}/10</li>
-			<li>Box Office: {filmDetails.BoxOffice}</li>
-			{
-				filmDetails.Website && (
-					<li><a href={filmDetails.Website}>Official website</a></li>
-				)
-			}
-			<li><a href={imdbUrl}>{'IMDB page'}</a></li>
-		</ul>
-		<br />
-		<a href="#top">Top</a>
-	</div>
+export class FilmDetail extends React.Component {
+	constructor(props) {
+		super(props)
+		this.keyDownListener = this.keyDownListener.bind(this)
+	}
+
+	keyDownListener(ev) {
+		if (ev.keyCode === ESC_KEY) {
+			this.props.dispatchViewList()
+		}
+	}
+
+	componentDidMount() {
+		document.addEventListener('keydown', this.keyDownListener)
+	}
+
+	componentWillUnmount() {
+		document.removeEventListener('keydown', this.keyDownListener)
+	}
+
+	render() {
+		const {
+			filmSummary,
+			filmDetails,
+			dispatchViewList
+		} = this.props
+		const imdbUrl = `https://www.imdb.com/title/${filmDetails.imdbID}`
+		return (
+			<div style={wrapperStyle}>
+				<a name="top" />
+				<div style={headerStyle}>
+					<h1 style={titleStyle}>{filmSummary.Title}</h1>
+					<a
+						href='#'
+						onClick={() => dispatchViewList()}
+						id="close-button"
+						title="Close"
+						style={closeButtonStyle}
+					>{'Close'}</a>
+				</div>
+				<img src={filmSummary.Poster} />
+				<ul style={detailsStyle}>
+					<li>{filmSummary.Year}</li>
+					<li>Directed by {filmDetails.Director}</li>
+					<li>Written by {filmDetails.Writer}</li>
+					<li>Cast: {filmDetails.Actors}</li>
+					<li>Language: {filmDetails.Language}</li>
+					{
+						filmDetails.Awards && <li>Awards: {filmDetails.Awards}</li>
+					}
+					<li>Run Time: {filmDetails.Runtime}</li>
+					<li>IMDB Rating: {filmDetails.imdbRating}/10</li>
+					<li>Box Office: {filmDetails.BoxOffice}</li>
+					{
+						filmDetails.Website && (
+							<li><a href={filmDetails.Website}>Official website</a></li>
+						)
+					}
+					<li><a href={imdbUrl}>{'IMDB page'}</a></li>
+				</ul>
+				<br />
+				<a href="#top">Top</a>
+			</div>
+		)
+	}
 }
 
 FilmDetail.propTypes = {
