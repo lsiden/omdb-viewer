6e8462fb18b913ec023b0979747883e6e5044d3b
Lawrence Siden <lsiden@gmail.com>
2018-03-15 09:29:21 -0400

    Installs electron-packager

--- src/util/require-remote.js
+++ src/util/require-remote.js
@@ -1,4 +1,4 @@
-// Require a module from the nodejs/electron runtime, not the Webpack bundle.
+// Require a module from the nodejs/electron main process, not the Webpack bundle.
 // This can expose the app to security risks.
 // The called must be careful not to load code from untrusted sources.
 // path should be to a library we have installed and trust.
