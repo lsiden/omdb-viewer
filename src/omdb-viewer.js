710a47931c9c826eeb2d6a92cd3542c1eeaaac77
Lawrence Siden <lsiden@gmail.com>
2018-03-20 14:49:42 -0400

    Renames OMD_URL

--- src/omdb-viewer.js
+++ src/omdb-viewer.js
@@ -7,7 +7,7 @@ import QueryForm from "./query-form"
 import FilmList from "./film-list"
 import FilmDetail from "./film-detail"
 import Show from "components/show"
-import { OMD_URL } from "./constants"
+import { OMDB_URL } from "./constants"
 
 const linkStyle = {
   color: "white",
@@ -26,7 +26,7 @@ const renderBanner = () => (
   <header style={headerStyle}>
     <h1 style={titleStyle}>
       {"Search Open Movie Database"}&nbsp;
-      <a href={OMD_URL} style={linkStyle}>
+      <a href={OMDB_URL} style={linkStyle}>
         (OMDB)
       </a>
     </h1>
