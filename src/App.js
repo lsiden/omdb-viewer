f8d343faf3d61d8081f33ccf3bfeb3360d8ea63f
Lawrence Siden <lsiden@gmail.com>
2018-04-17 13:15:00 -0400

    Refactors store

--- src/App.js
+++ src/App.js
@@ -1,33 +1,8 @@
 import React from "react"
-import { createStore, applyMiddleware, compose } from "redux"
 import { Provider } from "react-redux"
-import thunk from "redux-thunk"
 
 import OmdbSearch from "./omdb-viewer"
-import { Actions } from "./actions"
-
-const initialState = {
-  view: Actions.VIEW_FILM_LIST,
-}
-
-export function reduce(state = initialState, action) {
-  const { type, data } = action
-  switch (type) {
-    case Actions.VIEW_FILM_LIST:
-    case Actions.VIEW_FILM_DETAIL:
-    case Actions.UPDATE_FILMS:
-    case Actions.UPDATE_FILM_DETAILS:
-      return {
-        ...state,
-        ...data,
-      }
-    default:
-      return state
-  }
-}
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-const store = createStore(reduce, composeEnhancers(applyMiddleware(thunk)))
+import store from "./store"
 
 const App = () => (
   <Provider store={store}>
---
+++ src/store.js
@@ -0,0 +1,29 @@
+import { createStore, applyMiddleware, compose } from "redux"
+import thunk from "redux-thunk"
+
+import { Actions } from "./actions"
+
+const initialState = {
+  view: Actions.VIEW_FILM_LIST,
+}
+
+export function reduce(state = initialState, action) {
+  const { type, data } = action
+  switch (type) {
+    case Actions.VIEW_FILM_LIST:
+    case Actions.VIEW_FILM_DETAIL:
+    case Actions.UPDATE_FILMS:
+    case Actions.UPDATE_FILM_DETAILS:
+      return {
+        ...state,
+        ...data,
+      }
+    default:
+      return state
+  }
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(reduce, composeEnhancers(applyMiddleware(thunk)))
+
+export default store
