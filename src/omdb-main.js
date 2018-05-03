822e2155539e0c0764005f82faa4312ee050a6a4
Lawrence Siden <lsiden@gmail.com>
2018-05-03 15:42:53 -0400

    Adds FIXME comment

--- src/omdb-main.js
+++ src/omdb-main.js
@@ -7,7 +7,10 @@ import FilmDetail from "film-detail"
 import About from "about"
 import store from "actions/store"
 
-// See https://itnext.io/so-you-want-to-host-your-single-age-react-app-on-github-pages-a826ab01e48
+// FIXME - this may no longer be needed after Github enabled HTTPS to github pages
+// See https://blog.github.com/2018-05-01-github-pages-custom-domains-https/
+
+// See  https://itnext.io/so-you-want-to-host-your-single-age-react-app-on-github-pages-a826ab01e48
 // for basename property.
 export const OmdbMainUnconnected = () => (
   <BrowserRouter basename={process.env.PUBLIC_URL}>
