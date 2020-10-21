0f9faffb1b63b015b763557b3707ae7fff0c6859
Lawrence Siden <lsiden@gmail.com>
2020-10-21 10:03:16 -0400

    Updates

---
+++ precache-manifest.08f2e76ced669bc79515359be947632f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3449fb8daff975d5ee2de45ccc7a7f7e",
+    "url": "/omdb-film-browser-web/index.html"
+  },
+  {
+    "revision": "2854c187339fb3b9f41d",
+    "url": "/omdb-film-browser-web/static/css/2.8ec0c4ed.chunk.css"
+  },
+  {
+    "revision": "2854c187339fb3b9f41d",
+    "url": "/omdb-film-browser-web/static/js/2.bd8ac93c.chunk.js"
+  },
+  {
+    "revision": "5bba1771fd5b4a2319d369ca22fd2d31",
+    "url": "/omdb-film-browser-web/static/js/2.bd8ac93c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78669d62e4bbddf321d5",
+    "url": "/omdb-film-browser-web/static/js/main.ce0f5ebc.chunk.js"
+  },
+  {
+    "revision": "e0a8c98cccc432fa39cd",
+    "url": "/omdb-film-browser-web/static/js/runtime-main.0ecbfc25.js"
+  }
+]);
